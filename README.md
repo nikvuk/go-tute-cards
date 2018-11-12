ac534cb6fd5f02e815cb86422a704f3de55dfc0f
Nik Vukovljak <nvukovlj@zeus.local>
2018-11-12 21:24:23 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# go-tute-cards
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	cards := newDeck()
+
+	//hand, remainingDeck := deal(cards, 5)
+
+	//hand.print()
+	//remainingDeck.print()
+
+	//fmt.Println(cards.toString())
+
+	cards.saveToFile("my_cards")
+	cards = newDeckFromFile("my_cards")
+	cards.print()
+
+}
